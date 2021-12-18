2c95729f946b319f5975efbe87bb96fa747f0a50
anugrahjaya1 <anugrahjaya23@gmail.com>
2021-12-18 11:39:45 +0700

    comment code didn't use

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -70,7 +70,7 @@
                 </a>
                 <div id="collapseUserManagement" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item" href="{{url('/users')}}">User</a>
+                        <a class="collapse-item" href="{{route('users.index')}}">User</a>
                         <a class="collapse-item" href="{{url('/roles')}}">Role</a>
                         <a class="collapse-item" href="{{url('/permissions')}}">Permission</a>
                     </div>
@@ -122,16 +122,7 @@
 
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
-
-                    <!-- Page Heading -->
-                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-                    </div>
-
-                    <div class="row">
                         @yield('content')
-                    </div>
-
                 </div>
                 <!-- /.container-fluid -->
 
